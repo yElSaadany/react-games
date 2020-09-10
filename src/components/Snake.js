66de5db23024ca070065f0a4c24fc42530958841
Youssef El Saadany <youssef.saadany@gmail.com>
2020-09-10 15:23:42 +0200

    Updated Snake.js to add game loop logic & directions

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -6,27 +6,93 @@ export const SnakePart = (props) => {};
 
 export const Snake = (props) => {
   const canvasRef = useRef(null);
-  const [snake, setSnake] = useState([[5, 5]]);
+  const [ctx, setContext] = useState(null);
+  const [time, setTime] = useState(null);
+  const [direction, setDirection] = useState("UP");
+  const [snake, setSnake] = useState([
+    [15, 10],
+    [14, 10],
+    [13, 10],
+  ]);
 
   const drawSnake = (ctx) => {
     ctx.fillStyle = "green";
+    console.log("DRAWN SNAKE " + snake);
     snake.map((part) => {
-      console.log(snake);
-      ctx.fillRect(part[0] * 30, part[1] * 30, 30, 30);
+      console.log("X " + part[0] + " Y " + part[1]);
+      ctx.fillRect(part[0] * 30, part[1] * 30, 29, 29);
     });
   };
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+  const moveSnake = () => {
+    if (direction == "RIGHT") {
+      setSnake((prev) => {
+        let tmp = [...prev];
+        console.log("PREVIOUS SNAKE " + tmp);
+        const head = tmp[0];
+        const newHead = [head[0] + 1, head[1]];
+        tmp.pop();
+        tmp.unshift(newHead);
+        console.log("NEW SNAKE " + tmp);
+        return tmp;
+      });
+    } else if (direction == "LEFT") {
+      setSnake((prev) => {
+        let tmp = [...prev];
+        console.log("PREVIOUS SNAKE " + tmp);
+        const head = tmp[0];
+        const newHead = [head[0] - 1, head[1]];
+        tmp.pop();
+        tmp.unshift(newHead);
+        console.log("NEW SNAKE " + tmp);
+        return tmp;
+      });
+    } else if (direction == "UP") {
+      setSnake((prev) => {
+        let tmp = [...prev];
+        console.log("PREVIOUS SNAKE " + tmp);
+        const head = tmp[0];
+        const newHead = [head[0], head[1] - 1];
+        tmp.pop();
+        tmp.unshift(newHead);
+        console.log("NEW SNAKE " + tmp);
+        return tmp;
+      });
+    } else {
+      setSnake((prev) => {
+        let tmp = [...prev];
+        console.log("PREVIOUS SNAKE " + tmp);
+        const head = tmp[0];
+        const newHead = [head[0], head[1] + 1];
+        tmp.pop();
+        tmp.unshift(newHead);
+        console.log("NEW SNAKE " + tmp);
+        return tmp;
+      });
+    }
+  };
 
-    context.fillStyle = "white";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    drawSnake(context);
-  }, [snake]);
+  useEffect(() => {
+    if (time == null) {
+      const interval = setInterval(() => setTime(Date.now), 500);
+    } else {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "white";
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      drawSnake(ctx);
+      moveSnake();
+    }
+  }, [time]);
 
   return (
-    <canvas ref={canvasRef} className="game-container" {...props}></canvas>
+    <canvas
+      ref={canvasRef}
+      className="game-container"
+      {...props}
+      width="900px"
+      height="600px"
+    ></canvas>
   );
 };
 
