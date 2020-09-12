de87da7bebf3ab353292f1f52c0c095f5d8b731c
Youssef El Saadany <youssef.saadany@gmail.com>
2020-09-12 23:59:54 +0200

    Updated App.js to use as the Snake game container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Snake from "./components/Snake";
 
 import "./App.css";
 
 function App() {
+  const [play, setPlay] = useState(false);
+  const gameOver = (score) => {
+    setPlay(false);
+    console.log(score);
+  };
   return (
     <div className="App">
-      <Snake />
+      {!play ? (
+        <button onClick={() => setPlay(true)}>Play Game</button>
+      ) : (
+        <Snake gameOver={gameOver} />
+      )}
     </div>
   );
 }
